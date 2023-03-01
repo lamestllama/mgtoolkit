1e2043adcc8666961a1c9e3ac204be1d5f19734c
lamestllama <eric@eparsonage.com>
2023-03-01 20:03:56 +1030

    Bump version: 1.1.1 → 1.1.2

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ requirements = [
 # noinspection PyPep8
 setup(
     name='mgtoolkit',
-    version='1.1.1',
+    version='1.1.2',
     description="This is a Python package for implementing metagraphs.",
     long_description=readme + '\n\n' + history,
     author="Eric Parsonage",
